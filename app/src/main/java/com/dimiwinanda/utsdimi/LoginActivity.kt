ba77015436861c311fe50a09f2a0723e6602c755
Dimi Winanda <102844406+dimiwinanda1@users.noreply.github.com>
2022-04-04 17:58:33 +0700

    Done

--- app/src/main/java/com/dimiwinanda/utsdimi/LoginActivity.kt
+++ app/src/main/java/com/dimiwinanda/utsdimi/LoginActivity.kt
@@ -1,20 +1,30 @@
 package com.dimiwinanda.utsdimi
-
+//untuk mengatasi error pada DisplayMessageActivity kita bisa menggunakan sendMessage.
+//pada sendMessage konstruktor intent kita menggunakan 2 parameter, yaitu Context dan Class
+//terdapat import android.content.Intent dan import android.os.Bundle; yang berfungsi untuk 
+//membaca Data dengan  tipe berbentuk String, angka (integer, float, double), ArrayList, boolean, array, character, dan lain sebagainya.
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dimiwinanda.utsdimi.databinding.ActivityLoginBinding
 
+//AppCompat Library biasanya digunakan untuk mengimplementasikan ActionBar/Toolbar dan 
+//Material Design pada aplikasi yang kita buat di Android Studio. 
+//akan tetapi saat ini sudah menjadi bawaan saat kita membuat project. 
+
 class LoginActivity : AppCompatActivity() {
-
+    
+    //inisialisasi variable binding terlebih dahulu diluar fungsi onCreate().
     private lateinit var binding: ActivityLoginBinding
-
+    
+    //Untuk menginisiasi/membuat activity
+    //Method onCreate menerima parameter savedInstanceState, yang merupakan objek Bundle yang berisi status aktivitas yang sebelumnya disimpan
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // ketikan btn login telah di klik maka akan menuju ke halaman home
+        // ketika button login di klik maka akan menuju ke halaman home
         binding.btnLogin.setOnClickListener {
             startActivity(Intent(this, HomeActivity::class.java))
         }
