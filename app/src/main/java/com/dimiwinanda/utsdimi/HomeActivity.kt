b48d2e8f8a923e0ba830763e3fe493c97d016db0
Dimi Winanda <102844406+dimiwinanda1@users.noreply.github.com>
2022-04-04 18:17:20 +0700

    Done

--- app/src/main/java/com/dimiwinanda/utsdimi/HomeActivity.kt
+++ app/src/main/java/com/dimiwinanda/utsdimi/HomeActivity.kt
@@ -1,15 +1,15 @@
 package com.dimiwinanda.utsdimi
 //untuk mengatasi error pada DisplayMessageActivity kita bisa menggunakan sendMessage.
-//pada sendMessage konstruktor intent kita menggunakan 2 parameter, yaitu Context dan Class
+//pada konstruktor intent kita menggunakan 2 parameter, yaitu Context dan Class
 //terdapat import android.content.Intent dan import android.os.Bundle; yang berfungsi untuk 
-//membaca Data dengan  tipe berbentuk String, angka (integer, float, double), ArrayList, boolean, array, character, dan lain sebagainya, 
+//membaca Data dengan  tipe String, angka (integer, float, double), ArrayList, boolean, array, character, dan lain sebagainya, 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dimiwinanda.utsdimi.databinding.ActivityHomeBinding
 
 //AppCompat Library biasanya digunakan untuk mengimplementasikan ActionBar/Toolbar dan 
-//Material Design pada aplikasi yang kita buat di Android Studio. 
+//Material Design pada aplikasi yang kita buat di Android Studio,
 //akan tetapi saat ini sudah menjadi bawaan saat kita membuat project. 
 class HomeActivity : AppCompatActivity() {
     
