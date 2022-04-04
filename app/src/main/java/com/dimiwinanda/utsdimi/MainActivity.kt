93c5d37d10f452818cbe0923839c5678dcc2f575
Dimi Winanda <102844406+dimiwinanda1@users.noreply.github.com>
2022-04-04 18:01:42 +0700

    Done

--- app/src/main/java/com/dimiwinanda/utsdimi/MainActivity.kt
+++ app/src/main/java/com/dimiwinanda/utsdimi/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.dimiwinanda.utsdimi
 
+//untuk mengatasi error pada DisplayMessageActivity kita bisa menggunakan sendMessage.
+//pada sendMessage konstruktor intent kita menggunakan 2 parameter, yaitu Context dan Class
+//terdapat import android.content.Intent dan import android.os.Bundle; yang berfungsi untuk 
+//membaca Data dengan  tipe berbentuk String, angka (integer, float, double), ArrayList, boolean, array, character, dan lain sebagainya.
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dimiwinanda.utsdimi.databinding.ActivityMainBinding
 
+//AppCompat Library biasanya digunakan untuk mengimplementasikan ActionBar/Toolbar dan 
+//Material Design pada aplikasi yang kita buat di Android Studio. 
+//akan tetapi saat ini sudah menjadi bawaan saat kita membuat project. 
 class MainActivity : AppCompatActivity() {
 
+    //inisialisasi variable binding terlebih dahulu diluar fungsi onCreate().
     private lateinit var binding: ActivityMainBinding
 
+    //Untuk menginisiasi/membuat activity
+    //Method onCreate menerima parameter savedInstanceState, yang merupakan objek Bundle yang berisi status aktivitas yang sebelumnya disimpan.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
